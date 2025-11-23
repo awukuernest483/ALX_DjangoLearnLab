fcb09ecfaa4f4fa9c68ee62dfa9a5fe3c2a01fc1
Ernest Awuku Junior <awukuernest@gmail.com>
2025-11-23 23:38:17 +0000

    changes

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,10 @@
-from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(generics.ListAPIView):
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides standard CRUD actions for the Book model.
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
